d6e36139001d76a88c759c46004f2f52d1415de7
yifang <2424004764@qq.com>
2020-11-04 18:50:31 +0800

    确定好是以四层的架构来支撑阅读器核心部分

---
+++ pages/start-read/start-read.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="body">
+		<!-- 阅读器分四层 每层分别是： -->
+		<!-- 用以承载书籍章节内容 第一层 -->
+		<div class="read-layer layer"></div>
+		
+		<!-- 用来控制上一页、下一页、调出菜单的层  为第二层-->
+		<div class="controller-layer layer"></div>
+	
+		<!-- 全屏层  用来弹出菜单后关闭菜单层的作用  为第三层 -->
+		<div class="close-controller-layer layer"></div>
+		
+		<!-- 菜单层  返回、章节、设置都在这一层  为第四层 -->
+		<div class="menu-layer layer"></div>
+	
+	</view>
+</template>
+
+<script>
+	import {getBookDetailUtil} from '@/util/function/book/book.js'
+	
+	export default {
+		name: "startRead",
+		data() {
+			return {
+				book_id: null,
+				bookDetail: null, // 书籍详情
+			}
+		},
+		onLoad(options){
+			this.book_id = options.book_id
+		},
+		methods: {
+			getBookDetail(){
+				getBookDetailUtil(this.book_id).then(res => {
+					this.bookDetail = res
+					// 设置页面标题
+					uni.setNavigationBarTitle({
+					    title: res.book_name
+					})
+				})
+			},
+		},
+		mounted(){
+			this.getBookDetail()
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+.body{
+	
+}
+</style>
---
+++ util/function/book/book.js
@@ -0,0 +1,22 @@
+import {getBookDetail} from '@/util/user_http/book.js'
+
+/**
+ * 公共的获取书籍详情的方法
+ * @param {Object} book_id 书籍id
+ */
+export function getBookDetailUtil(book_id){
+	return getBookDetail({
+		book_id: book_id
+	}).then(res => {
+		// 处理书籍封面图
+		try{
+			res.data['book_cover_imgs'] = JSON.parse(res.data['book_cover_imgs'])
+		}catch(err){
+			// 图片为空
+			res.data['book_cover_imgs'] = []
+		}
+		return Promise.resolve(res.data)
+	}).catch(err => {
+		return Promise.reject(err)
+	})
+}
