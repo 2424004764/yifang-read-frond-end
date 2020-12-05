162d88b48bf5ddf00ffa8cded69ea3c47663625a
yifang <2424004764@qq.com>
2020-12-05 21:41:44 +0800

    perf 使用https协议

--- util/config.js
+++ util/config.js
@@ -1,6 +1,6 @@
 // 挂载到全局  使用参考/pages/about/abut
 module.exports = {
-	baseUrl: 'http://yifangread.fologde.com', // api 接口地址
+	baseUrl: 'https://yifangread.fologde.com', // api 接口地址
 	adminQQ: '2424004764', // 管理员QQ
 	appName: '一方阅读', // app 名字
 	version: '1.0.1', // app 版本
