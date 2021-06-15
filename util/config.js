1dea6f85ab697fffec378050aabf2663c97af2cf
一方 <2424004764@qq.com>
2021-06-15 10:16:37 +0800

    Update config.js

--- util/config.js
+++ util/config.js
@@ -1,7 +1,7 @@
 // 挂载到全局  使用参考/pages/about/abut
 module.exports = {
 	// baseUrl: 'http://192.168.43.203:885/web/index.php', // 本地 api 接口地址
-	baseUrl: 'https://yifang-read-api.fologde.com/web/index.php', // api 接口地址
+	baseUrl: '', // api 接口地址
 	adminQQ: '2424004764', // 管理员QQ
 	adminEmail: 'yifangread@163.com', // 管理员邮箱
 	appName: '一方知书', // app 名字  本来想叫一方阅读  结果被占用了
